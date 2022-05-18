4cad70eab54c371999dd4bd9fce4aff6dbb3e470
Tipu1996 <67307877+Tipu1996@users.noreply.github.com>
2022-05-18 05:31:11 +0300

    Add files via upload

---
+++ Google_Maps.py
@@ -0,0 +1,37 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+api_key = False
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+address = input('Enter location: ')
+if len(address) < 1: exit()
+
+parms = dict()
+parms['address'] = address
+if api_key is not False: parms['key'] = api_key
+url = serviceurl + urllib.parse.urlencode(parms)
+
+uh = urllib.request.urlopen(url, context=ctx)
+data = uh.read().decode()
+
+try:
+    js = json.loads(data)
+except:
+    js = None
+
+if not js or 'status' not in js or js['status'] != 'OK':
+    print('==== Failure To Retrieve ====')
+
+print(json.dumps(js["results"][0]["formatted_address"], indent=4))
